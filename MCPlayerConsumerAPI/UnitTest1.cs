32c0aaf81efb221383ec3f9bb0216ff613844b8a
WrexBG <40688355+WrexBG@users.noreply.github.com>
2022-11-30 13:35:11 +0100

    Added an empty test

---
+++ MCPlayerConsumerAPI/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MCPlayerConsumerAPI
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
