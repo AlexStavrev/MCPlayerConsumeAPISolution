aace2495390777a51097eddd6a85fe77b5217e5a
WrexBG <40688355+WrexBG@users.noreply.github.com>
2022-12-05 14:07:27 +0100

    Update PlayerDto.cs
    Updated to using file namespace

--- MCPlayerClassLibrary/DTOs/PlayerDto.cs
+++ MCPlayerClassLibrary/DTOs/PlayerDto.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace MCPlayerApiClient.DTOs
+namespace MCPlayerApiClient.DTOs;
+
+public sealed class PlayerDto
 {
-    public sealed class PlayerDto
-    {
-        public string Name { get; set; }
-        public Guid Id { get; set; }
-        public IEnumerable<NameChangeDto> Names { get; set; }
-    }
+    public string Name { get; set; }
+    public Guid Id { get; set; }
+    public IEnumerable<NameChangeDto> Names { get; set; }
 }
